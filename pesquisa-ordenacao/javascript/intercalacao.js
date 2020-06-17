b7236ca904ec8cc1bf58accd5249602421b16133
Gilson Filho <gilsonpcf@gmail.com>
2020-06-17 18:42:45 -0300

    Update intercalacao.js

--- pesquisa-ordenacao/javascript/intercalacao.js
+++ pesquisa-ordenacao/javascript/intercalacao.js
@@ -1,21 +1,23 @@
 // Pesquisa e ordenação
-// Descrição : Intercalação bidirecional
+// Descrição : Intercalação de duas vias
 // Problema  : Mesclar dois vetores de inteiros, ambos com seus elementos
-               em ordem crescente, em um único vetor ordenado.
+//             em ordem crescente, em um único vetor ordenado.
 // Aplicações: Ordenação, processamento de dados.
 
-var a = [15, 18, 42, 51];
-var b = [8, 11, 16, 17, 44, 58, 71, 74];
-var c = [];
-var tamanho = a.length + b.length;
-var i = 0;
-var j = 0;
+var a = [15, 18, 42, 51],
+    b = [8, 11, 16, 17, 44, 58, 71, 74],
+    c = [];
 
-for (var k = 0; k < tamanho; k++) {
+var m = a.length - 1,
+    n = b.length - 1;
+
+var i = 0,
+    j = 0,
+    k = 0;
+
+while (i <= m && j <= n) {
     // Compara dois elementos de A e B, e inclui o menor deles em C
-    // A condição j == b.length fará com que os elementos restantes
-    // de A ou de B sejam copiados para C
-    if (a[i] < b[j] || j == b.length) {
+    if (a[i] < b[j]) {
         c[k] = a[i];
         i++;
     }
@@ -23,6 +25,22 @@ for (var k = 0; k < tamanho; k++) {
         c[k] = b[j];
         j++;
     }
+
+    k++;
+}
+
+// Copia para C os elementos restantes de A
+while (i <= m) {
+    c[k] = a[i];
+    i++;
+    k++;
+}
+
+// Copia para C os elementos restantes de B
+while (j <= n) {
+    c[k] = b[j];
+    j++;
+    k++;
 }
 
 document.write(c); // Exibe os elementos de C
