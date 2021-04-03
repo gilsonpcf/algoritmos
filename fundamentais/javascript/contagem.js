fd0f98965b02a023f7052b5cf423cdd3d4b6507a
Gilson Pereira <gilsonpcf@gmail.com>
2021-04-03 00:17:53 -0300

    Update contagem.js

--- fundamentais/javascript/contagem.js
+++ fundamentais/javascript/contagem.js
@@ -1,23 +1,24 @@
 // Algoritmo Fundamental Nº 2
-// Descrição : Contagem.
-// Problema  : Dado um conjunto de N notas de um exame, fazer uma contagem do
-//             número de alunos que passaram no exame. Passará o aluno que
-//             tirar nota 50 ou maior (no intervalo de 0 a 100).
+// Descrição : Contagem
+// Problema  : Dado um conjunto de n notas de alunos em um exame,
+//             fazer uma contagem do número de alunos que foram
+//             aprovados no exame. Será considerado aprovado o aluno
+//             que tirar nota 50 ou maior (no intervalo de 0 a 100).
 // Aplicações: Todas as formas de contagem.
 
-var n = prompt("Informe a quantidade notas:");
+var n = prompt("Número de notas:");
 var nota;
-var contador = 0;
+var contagem = 0;
 var i = 0;
 
 while (i < n) {
-	nota = prompt("Informe uma nota");
+	i = i + 1;
+
+	nota = prompt("Nota do aluno:");
 
 	if (nota >= 50) {
-		contador = contador + 1;
+		contagem = contagem + 1;
 	}
-	
-	i = i + 1;
 }
 
-document.write("Número de alunos que passaram no exame: " + contador);
+document.write("Número de aprovações: " + contagem);
