a98a219ab2e2c6b744c4b1e84da0c6af285c660a
Gilson Pereira <gilsonpcf@gmail.com>
2021-04-03 00:18:38 -0300

    Update somatorio.js

--- fundamentais/javascript/somatorio.js
+++ fundamentais/javascript/somatorio.js
@@ -1,18 +1,18 @@
 // Algoritmo Fundamental Nº 3
-// Descrição : Soma de um conjunto de números.
-// Problema  : Dado um conjunto de N números, calcular a soma desses números.
-//             Assumir que N é maior ou igual a zero.
-// Aplicações: Cálculos de média, variância e mínimos quadrados.
+// Descrição : Soma de um conjunto de números
+// Problema  : Dado um conjunto de n números, calcular a soma desses números.
+//             Assumir que n é maior ou igual a zero.
+// Aplicações: Cálculos de média, cálculos de variância e mínimos quadrados.
 
-var n = prompt("Informe a quantidade de números:");
+var n = prompt("Quantidade de números:");
 var numero;
 var soma = 0;
 var i = 0;
 
 while (i < n) {
-	numero = parseInt(prompt("Informe um número"));
-	soma = soma + numero;
 	i = i + 1;
+	numero = parseInt(prompt("Número:"));
+	soma = soma + numero;
 }
 
-document.write("A soma dos " + n + " números é " + soma);
+document.write("Soma = " + soma);
