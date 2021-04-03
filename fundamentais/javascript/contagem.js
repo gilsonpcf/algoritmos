c68b255b67013e7e2cf109ae1a051918783fd1e7
Gilson Pereira <gilsonpcf@gmail.com>
2021-04-03 00:20:51 -0300

    Update contagem.js

--- fundamentais/javascript/contagem.js
+++ fundamentais/javascript/contagem.js
@@ -12,13 +12,13 @@ var contagem = 0;
 var i = 0;
 
 while (i < n) {
-	i = i + 1;
+    i = i + 1;
 
-	nota = prompt("Nota do aluno:");
+    nota = prompt("Nota do aluno:");
 
-	if (nota >= 50) {
-		contagem = contagem + 1;
-	}
+    if (nota >= 50) {
+        contagem = contagem + 1;
+    }
 }
 
 document.write("Número de aprovações: " + contagem);
