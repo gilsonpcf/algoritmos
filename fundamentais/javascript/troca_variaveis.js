aef7aac443c0a3a781f5749bee1b2d6a2eb2f6ab
Gilson Pereira <gilsonpcf@gmail.com>
2021-04-03 00:17:13 -0300

    Update troca_variaveis.js

--- fundamentais/javascript/troca_variaveis.js
+++ fundamentais/javascript/troca_variaveis.js
@@ -1,15 +1,15 @@
 // Algoritmo Fundamental Nº 1
-// Descrição : Troca dos valores de duas variáveis.
+// Descrição : Troca dos valores de duas variáveis
 // Problema  : Dadas duas variáveis, trocar os valores atribuídos a elas.
 // Aplicações: Algoritmos de ordenação.
 
-var a = prompt("Informe valor para a variável A:");
-var b = prompt("Informe valor para a variável B:");
+var a = prompt("Valor da variável A:");
+var b = prompt("Valor da variável B:");
 var aux;
 
-aux = a; // Guarda em AUX o valor original de A
-a = b;   // Atribui a A o valor original de B
-b = aux; // Atribui a B o valor original de A, que está armazenado em AUX
+aux = a;
+a = b;
+b = aux;
 
 document.write("Novo valor da variável A: " + a + "<br>");
 document.write("Novo valor da variável B: " + b);
