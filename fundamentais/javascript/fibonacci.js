1cb187b949688aa432063ba35069d1092fb846ee
Gilson Pereira <gilsonpcf@gmail.com>
2021-04-03 00:25:13 -0300

    Update fibonacci.js

--- fundamentais/javascript/fibonacci.js
+++ fundamentais/javascript/fibonacci.js
@@ -1,22 +1,29 @@
 // Algoritmo Fundamental Nº 6
-// Descrição : Geração da sequência de Fibonacci.
-// Problema  : Gerar e imprimir os N primeiros termos da sequência de Fibonacci,
-//             onde N > 1. Os primeiros termos são: 0, 1, 1, 2, 3, 5, 8, 13, ...
-//             Cada termo, além dos dois primeiros, é derivado da soma de seus
-//             dois antecessores mais próximos.
-// Aplicações: Botânica, teoria da rede elétrica, classificação e pesquisa de dados.
+// Descrição : Geração da sequência de Fibonacci
+// Problema  : Gerar e imprimir os n primeiros termos da sequência
+//             de Fibonacci, onde n >= 1. Os primeiros termos são:
+//             0, 1, 1, 2, 3, 5, 8, 13, ...
+//             Cada termo, além dos dois primeiros, é derivado da
+//             soma de seus dois antecessores mais próximos.
+// Aplicações: Botânica, teoria das redes elétricas, ordenação e pesquisa.
 
-var n = prompt("Informe o número de termos da sequência:");
+var n = prompt("Número de termos:");
 
-var termo1 = 0;
-var termo2 = 1;
-var termo3;
+var a = 0;
+var b = 1;
+var i = 2;
+var c;
 
-document.write(termo1 + ", " + termo2);
+if (n == 1) {
+    document.write(a);
+} else {
+    document.write(a + ", " + b);
+}
 
-for (i = 3; i <= n; i++) {
-    termo3 = termo1 + termo2;
-    document.write(", " + termo3);
-    termo1 = termo2;
-    termo2 = termo3;
+while (i < n) {
+	i = i + 1;
+    c = a + b;
+    document.write(", " + c);
+    a = b;
+    b = c;
 }
