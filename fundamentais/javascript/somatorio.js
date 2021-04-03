a868360f58b6553f682c6a7c0b1ee215b30f902d
Gilson Pereira <gilsonpcf@gmail.com>
2021-04-03 00:21:48 -0300

    Update somatorio.js

--- fundamentais/javascript/somatorio.js
+++ fundamentais/javascript/somatorio.js
@@ -10,9 +10,9 @@ var soma = 0;
 var i = 0;
 
 while (i < n) {
-	i = i + 1;
-	numero = parseInt(prompt("Número:"));
-	soma = soma + numero;
+    i = i + 1;
+    numero = parseInt(prompt("Número:"));
+    soma = soma + numero;
 }
 
 document.write("Soma = " + soma);
