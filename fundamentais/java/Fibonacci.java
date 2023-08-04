b99809e1955f904b4c63c65ed6a3d100d0283c64
Gilson Filho <gilsonpcf@gmail.com>
2023-08-04 16:36:04 -0300

    Update Fibonacci.java

--- fundamentais/java/Fibonacci.java
+++ fundamentais/java/Fibonacci.java
@@ -1,23 +1,38 @@
+// Algoritmo Fundamental Nº 6
+// Descrição : Geração da sequência de Fibonacci
+// Problema  : Gerar e imprimir os n primeiros termos da sequência
+//             de Fibonacci, onde n >= 1. Os primeiros termos são:
+//             0, 1, 1, 2, 3, 5, 8, 13, ...
+//             Cada termo, além dos dois primeiros, é derivado da
+//             soma de seus dois antecessores mais próximos.
+// Aplicações: Botânica, teoria das redes elétricas, ordenação e pesquisa.
+
 import java.util.Scanner;
 
 public class Fibonacci {
     public static void main(String[] args) {
         Scanner dado = new Scanner(System.in);
 
-        System.out.print("Informe o número de termos da sequência: ");
+        System.out.print("Número de termos: ");
         int n = dado.nextInt();
 
-        int termo1 = 0;
-        int termo2 = 1;
-        int termo3;
+        int a = 0;
+        int b = 1;
+        int i = 2;
+        int c;
 
-        System.out.print(termo1 + ", " + termo2);
+        if (n == 1) {
+            System.out.print(a);
+        } else {
+            System.out.print(a + " " + b);
+        }
 
-        for (int i = 3; i <= n; i++) {
-            termo3 = termo1 + termo2;
-            System.out.print(", " + termo3);
-            termo1 = termo2;
-            termo2 = termo3;
+        while (i < n) {
+            c = a + b;
+            System.out.print(" " + c);
+            a = b;
+            b = c;
+            i++;
         }
     }
 }
